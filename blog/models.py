639888e2a8303333bd479d5aadcf37d84cfe1260
Jakson Casas <jaksoncasas@gmail.com>
2021-09-16 16:26:31 -0500

    agregar atributo contenido en Post

--- blog/models.py
+++ blog/models.py
@@ -38,6 +38,7 @@ class Post(models.Model):
     titulo = models.CharField('Titulo',max_length=90,blank=False,null=False)
     slug = models.CharField('Slug',max_length=100,blank=False,null=False)
     descripcion = models.CharField('Descripcion',max_length=110,blank=False,null=False)
+    contenido = models.TextField('Contenido')
     imagen = models.URLField(max_length=255,blank=False,null=False)
     autor = models.ForeignKey(Autor,on_delete=models.CASCADE) #Llave Foranea
     categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
