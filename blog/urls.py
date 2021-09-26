dc95cf780178a226ae9e1820dd433b3bc1b09075
Jakson Casas <jaksoncasas@gmail.com>
2021-09-26 17:07:49 -0500

    Agregar la ruta para el post

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='index'),
+    path('<slug:slug>/',detallePost, name='detalle_post'),
     path('generales/',generales,name='generales'),
     path('turismo/',turismo,name='turismo'),
     path('tecnologia/',tecnologia,name='tecnologia'),
