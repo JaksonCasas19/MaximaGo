67f420579be5462e64d589f51d2ebb94df2eaff5
Jakson Casas <jaksoncasas@gmail.com>
2021-09-28 08:18:01 -0500

    Realizar una consulta en view y verificar si existe el post por medio de queryset

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,21 @@
 from blog.models import Post
 from django.shortcuts import render
 from .models import Categoria, Post
+from django.db.models import Q
 # Create your views here.
 
 def home(request):
+    #print(request.GET) /Para capturar en el buscador
+    queryset = request.GET.get("buscar")
     posts = Post.objects.filter(estado=True)
+
+    #Verificar si el texto pasado en el buscador existe
+    if queryset:
+        posts = Post.objects.filter(
+            Q(titulo = queryset) |
+            Q(descripcion = queryset)
+        ).distinct
+    
     return render(request,'index.html',{'posts':posts})
 
 def detallePost(request,slug):
@@ -15,7 +26,7 @@ def detallePost(request,slug):
     return render(request,'post.html',{'detalle_post':post})
 
 def generales(request):
-    posts = Post.objects.filter(estado=True,categoria= Categoria.objects.get(nombre__iexact='General'))
+    posts = Post.objects.filter(estado=True,categoria= Categoria.objects.get(nombre__iexact='General')) #Nombre__iexat Mayus y Minus
     return render(request,'generales.html',{'posts':posts})
 
 def turismo(request):
