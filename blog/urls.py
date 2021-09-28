dcf7c59c34c72959840e39637b529bb9e346b684
Jakson Casas <jaksoncasas@gmail.com>
2021-09-28 07:48:53 -0500

    Cambiar la posicion slug url al ultimo

--- blog/urls.py
+++ blog/urls.py
@@ -4,9 +4,9 @@ from .views import *
 
 urlpatterns = [
     path('',home,name='index'),
-    path('<slug:slug>/',detallePost, name='detalle_post'),
     path('generales/',generales,name='generales'),
     path('turismo/',turismo,name='turismo'),
     path('tecnologia/',tecnologia,name='tecnologia'),
     path('noticia/',noticia,name='noticia'),
+    path('<slug:slug>/',detallePost, name='detalle_post'),
 ]
